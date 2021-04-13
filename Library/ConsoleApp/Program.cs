ce88dc617d97cb134b96c84b5559548e748f493c
michaelolson1996 <michaelolson.1996@protonmail.com>
2021-04-12 23:03:53 -0600

    completed console library

---
+++ Library/ConsoleApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Library;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool quit = false;
+            string[] menuOptions = new string[] { "Open", "Save", "Print", "Quit" };
+            Form myForm = new Form();
+            CliLib myLib = new CliLib();
+
+            while (!quit)
+            {
+                myForm.Age = myLib.GetConsoleInt("What is your age?", 0, 110);
+                myForm.AreaCode = myLib.GetConsoleInt("What is your area code?", 0, 999);
+                myForm.ZipCode = myLib.GetConsoleInt("What is your zip code?", 0, 99999);
+                myForm.Number = myLib.GetConsoleInt("What is your favorite Number?", 0, 999999999);
+                myForm.FirstInitial = myLib.GetConsoleChar("What is your first initial?");
+                myForm.LastName = myLib.GetConsoleString("What is your last name?");
+                myForm.IsAwesome = myLib.GetConsoleBool("Are you awesome? [true/false]");
+                int option = myLib.GetConsoleMenu(menuOptions);
+
+                switch (option)
+                {
+                    case 1:
+                    case 2:
+                        {
+                            continue;
+                        }
+                    case 3:
+                        {
+                            myForm.Print();
+                            break;
+                        }
+                    case 4:
+                        {
+                            quit = true;
+                            break;
+                        }
+                }
+            }
+        }
+    }
+
+    class Form
+    {
+        public int Age { get; set; }
+        public int AreaCode { get; set; }
+        public int ZipCode { get; set; }
+        public int Number { get; set; }
+        public char FirstInitial { get; set; }
+        public bool IsAwesome { get; set; }
+        public string LastName { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine
+                (
+                $"Age : {this.Age}\n" +
+                $"Area Code : {this.AreaCode}\n" +
+                $"Zip Code : {this.ZipCode}\n" +
+                $"Number : {this.Number}\n" +
+                $"First Initial : {this.FirstInitial}\n" +
+                $"Is Awesome : {this.IsAwesome}\n" +
+                $"Last Name : {this.LastName}"
+                );
+        }
+    }
+}
---
+++ Library/Library/MyLibrary.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Library
+{
+    public class CliLib
+    {
+        public int GetConsoleInt(string message, int min, int max)
+        {
+            bool responding = true;
+            int number = 0;
+            while (responding)
+            {
+                Console.WriteLine(message);
+
+                if (!Int32.TryParse(Console.ReadLine(), out number) || number < min || number > max)
+                    Console.WriteLine($"Please enter a valid input, max={max}, min={min}");
+                else
+                    responding = false;
+            }
+
+            return number;
+        }
+
+        public bool GetConsoleBool(string message)
+        {
+            bool responding = true;
+            bool response = false;
+
+            while (responding)
+            {
+                Console.WriteLine(message);
+
+                if (!bool.TryParse(Console.ReadLine(), out response))
+                    Console.WriteLine("Please enter a valid input");
+                else
+                    responding = false;
+            }
+
+            return response;
+        }
+
+        public char GetConsoleChar(string message)
+        {
+            char userChar = 'a';
+            bool responding = true;
+
+            while (responding)
+            {
+                Console.WriteLine(message);
+
+                if (!char.TryParse(Console.ReadLine(), out userChar) || !Regex.IsMatch(userChar.ToString(), "[a-zA-Z]"))
+                    Console.WriteLine("Please enter a valid input");
+                else
+                    responding = false;
+            }
+
+            return userChar;
+        }
+
+        public string GetConsoleString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public int GetConsoleMenu(string[] items)
+        {
+            int count;
+            bool responding = true;
+            int number = 0;
+            while (responding)
+            {
+                count = 1;
+                foreach (string item in items)
+                {
+                    Console.WriteLine($"{count} - {item}");
+                    count++;
+                }
+                Console.Write("Enter Response : ");
+                string response = Console.ReadLine();
+
+                if (!Int32.TryParse(response, out number) && number > 0 && number < 5)
+                    Console.WriteLine("Please enter a valid input");
+                else
+                    return number;
+            }
+            return number;
+        }
+    }
+}
